c60e66a03faa29f9cbe98c1227559a1ada9dc7ba
sguha-work <sguha1988.life@gmail.com>
2017-09-11 14:48:37 +0530

    upto profile picture display done

--- src/pages/heading/heading.ts
+++ src/pages/heading/heading.ts
@@ -4,6 +4,7 @@ import {CommonService} from "./../../services/common.service";
 import { AfterViewInit } from '@angular/core';
 import { Events } from 'ionic-angular';
 import * as $ from 'jquery';
+import {User} from './../../interfaces/user.interface';
 @Component({
   selector: 'page-heading',
   templateUrl: 'heading.html',
@@ -18,16 +19,22 @@ export class HeadingPage  implements AfterViewInit{
     console.log('ionViewDidLoad HeadingPage');
   }
 
-  private displayPhoneNumber(phoneNumber: string) {
+  private displayProfilePicture(pictureData: string) {
+    if(pictureData !== "") {
+      $("page-heading #image_myProfile").attr('src', pictureData);
+    }
     
   }
 
-  private populateData(data: any) {
-    this.displayPhoneNumber(data.phoneNumbers[0]);
+  private populateData(data: User) {
+    this.displayProfilePicture(data.picture);
+    $("page-heading #image_contacts").show();
   }
 
   private bindEvents() {
-   
+   this.events.subscribe("USER-DETAILS-RECEIVED", (value) => {
+     this.populateData(value);
+   });
   }
 
   public displayContacts() {

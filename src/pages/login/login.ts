72490ff34c15598ff0a7de44c511b0b76f0d707e
sguha-work <sguha1988.life@gmail.com>
2017-09-25 15:09:12 +0530

    login work continues

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,18 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
 
 import { JoinLIFEPage } from '../join-life/join-life';
+
+import {CommonService} from "./../../services/common.service";
+import {LoginService} from "./../../services/login.service";
+import {AlertService} from "./../../services/alert.service";
+import {User} from "./../../interfaces/user.interface";
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
-export class LoginPage {
+export class LoginPage  implements AfterViewInit {
 
-  constructor(public navCtrl: NavController) {
+  private phoneNUmberDOM: any;
+  private passwordDOM: any;
+
+  constructor(public navCtrl: NavController,  private common: CommonService, private login: LoginService, private alertService: AlertService) {
   }
 
   gotoSignUp() {
     this.navCtrl.push(JoinLIFEPage);
+  }
+
+  beginLoginProcess() {
+
+  }
+
+  ngAfterViewInit() {
+    this.phoneNUmberDOM = $("page-login #txt_phoneNumber input");
+    this.passwordDOM = $("page-login #txt_password input");
   }
 }

409d8b6ef3bc8067b369349ed355b88b74357891
sguha-work <sguha1988.life@gmail.com>
2017-09-28 10:33:31 +0530

    image upload

---
+++ src/services/upload.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import * as firebase from 'firebase'; 
+import { AngularFireDatabase } from 'angularfire2/database';
+import { FirebaseListObservable } from 'angularfire2';
+
+@Injectable()
+export class UploadService {
+  constructor(private db: AngularFireDatabase) { }
+  private basePath:string = '/uploads';
+  uploads: FirebaseListObservable<Upload[]>;
+  pushUpload(upload: Upload) {
+    let storageRef = firebase.storage().ref();
+    let uploadTask = storageRef.child(`${this.basePath}/${upload.file.name}`).put(upload.file);
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+      (snapshot) =>  {
+        // upload in progress
+        upload.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      },
+      (error) => {
+        // upload failed
+        console.log(error)
+      },
+      () => {
+        // upload success
+        upload.url = uploadTask.snapshot.downloadURL
+        upload.name = upload.file.name
+        this.saveFileData(upload)
+      }
+    );
+  }
+  // Writes the file details to the realtime db
+  private saveFileData(upload: Upload) {
+    this.db.list(`${this.basePath}/`).push(upload);
+  }
+}

e86046ad08a391ce2e6412b931159a1a136edaa2
sguha-work <sguha1988.life@gmail.com>
2017-11-03 18:53:17 +0530

    login work begins

---
+++ src/services/login.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+
+import {Database} from './database.service';
+import {User} from "./../interfaces/user.interface";
+import {CommonService} from "./common.service";
+import {MessageService} from "./message.service";
+import {StorageService} from "./storage.service";
+
+@Injectable()
+export class LoginService {
+    
+}
